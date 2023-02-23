0661106f6c05e35edafb2fb1dd9080fd06554b5c
tforhad <69688144+tforhad@users.noreply.github.com>
2023-02-22 21:56:41 -0500

    Create WaldoFinderNeuralNetworkModue.py

---
+++ WaldoFinderNeuralNetworkModue.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch.optim as optim
+import torch
+import numpy as np
+import torch.nn.functional as F
+
+class WaldoFinder(nn.module):
+    def __init__(self):
+        super().__init__();
+
+    def forward(self, input):
+
+
+    
+        return input
